74c74dae7063c5081fbccabbb0038241dea8ad62
abedel <abedelmouamine.ben-ahmed@laplateforme.io>
2023-12-07 15:39:07 +0100

    update

--- générateur-password.py
+++ générateur-password.py
@@ -1,6 +1,7 @@
 import hashlib
 import json
 
+
 def caractère_speciaux(passwd):
     taille=len(passwd)-1
     for i in range(len(passwd)):
@@ -16,30 +17,33 @@ def caractère_speciaux(passwd):
 
 
 passwd=input('saisir un mot de passe: ')
-
-if len(passwd) >=8:
-    if passwd.isupper()==False:
-        if passwd.islower()==False:
-            if passwd.isalpha()==False:
-                if passwd.isdigit()==False:
-                    caractère_speciaux(passwd)
-                    hash_pass=hashlib.sha256(passwd.encode('utf-8'))
-                    print (hash_pass)
-                    ajout_base_de_donnée=input('voulez-vous ajouter votre mot de passe haché o/n :')
-                    if ajout_base_de_donnée == "o":
-                        mot_de_passe = open('Base_de_donnee.txt','a')
-                        son.dumps([1, 2, 3, {'4': 5, '6': 7}]
-                        mot_de_passe.write(hash_pass)
-                    elif ajout_base_de_donnée =="n" :
-                        print(json.dumps("\"foo\bar")
+while True:
+    if len(passwd) >=8:
+        if passwd.isupper()==False:
+            if passwd.islower()==False:
+                if passwd.isalpha()==False:
+                    if passwd.isdigit()==False:
+                        caractère_speciaux(passwd)
+                        hash_pass=hashlib.sha256(passwd.encode('utf-8')).hexdigest()
+                        ajout_data=input('voulez-vous ajouter votre mot de passe haché o/n :')
+                        if ajout_data == "o":
+                            utilite=input('Pour quel utilité aura votre mot de passe en un mot ')
+                            donnee_a_ajouter = {"user1" :{'utilite': utilite, 'password':hash_pass}}
+                            with open("data_mot-de-passe.json","w") as f:                
+                                json.dump(donnee_a_ajouter, f) 
+                                break                     
+                        elif ajout_data=='n':
+                            print('ok')
+                            break
+                    
+                    else:
+                        print('il faut plus de lettre ')
                 else:
-                    print('il faut plus de lettre ')
+                    print('il faut au moins un caractère numérique')       
             else:
-                print('il faut au moins un caractère numérique')       
+                print('il faut au moins un caractère majuscule')
         else:
-            print('il faut au moins un caractère majuscule')
+            print('il faut au moins un caractère minuscule')  
     else:
-        print('il faut au moins un caractère minuscule')  
-else:
-    print('le mot de passe doit contenir au moins 8 caractères' )
+        print('le mot de passe doit contenir au moins 8 caractères' )
 
